f5d9a4d322f51e3d0a9f15af604a594cca2d009d
Maikel Carballo <mcarballo@tutanota.com>
2025-04-26 16:29:46 -0400

    WIP feat: implementando gestión de roles

--- app/Http/Requests/Security/StoreRoleRequest.php
+++ app/Http/Requests/Security/StoreRoleRequest.php
@@ -11,7 +11,7 @@ class StoreRoleRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return $this->user()->can('create roles');
     }
 
     /**
@@ -22,7 +22,10 @@ class StoreRoleRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'max:255', 'unique:roles'],
+            'description' => ['required', 'string', 'lowercase'],
+            'guard_name' => ['required', 'string', ''],
+            'permissions' => ['required', 'string', ''],
         ];
     }
 }
