9cd2f523207ba153f5e611c00800fdc9e911ce31
Maikel Carballo <mcarballo@tutanota.com>
2025-07-15 23:39:46 -0400

    feat: implement advanced search and export to pdf in permissions

--- app/Actions/Security/Permission/ExportIndexToPdf.php
+++ app/Actions/Security/Permission/ExportIndexToPdf.php
@@ -2,12 +2,11 @@
 
 namespace App\Actions\Security\Permission;
 
-use App\Models\Debugging\ActivityLog;
 use App\Models\Organization\Organization;
 use App\Models\Security\Permission;
+use App\Models\Security\Role;
 use App\Support\DataExport\BasePdf;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
 
 class ExportIndexToPdf extends BasePdf
@@ -34,33 +33,40 @@ class ExportIndexToPdf extends BasePdf
         $this->setTextColor(255, 255, 255);
         $this->Cell(w: 0, txt: '1. FILTROS APLICADOS', border: 0, ln: 1, fill: true);
         $this->setTextColor(0, 0, 0);
-        $this->MultiCell(w: 40, h: 0, txt: 'Buscar', align: 'L', ln: 0);
+        $this->MultiCell(w: 40, h: 0, align: 'L', ln: 0, txt: 'Buscar');
         $this->setFont(family: 'iosevkafixedss12', size: 10);
-        $this->MultiCell(w: 0, h: 0, txt: $filters['search'] ?: 'Todo', ln: 1);
+        $this->MultiCell(w: 0, h: 0, align: 'L', ln: 1, txt: $filters['search'] ?: 'Todo');
         $this->setFont(family: 'helvetica', style: 'B', size: 10);
-        $this->MultiCell(w: 40, h: 0, txt: 'Usuario(s)', border: 'T', align: 'L', ln: 0);
+        $this->MultiCell(w: 40, h: 0, border: 'T', align: 'L', ln: 0, txt: 'Usuario(s)');
         $this->setFont(family: 'iosevkafixedss12', size: 10);
-        $this->MultiCell(w: 0, h: 0, txt: $filters['users'] ?: 'Todos', border: 'T', ln: 1);
+        $this->MultiCell(w: 0, h: 0, align: 'L', border: 'T', ln: 1, txt: $filters['users'] ?: 'Todos');
         $this->setFont(family: 'helvetica', style: 'B', size: 10);
-        $this->MultiCell(w: 40, h: 0, txt: 'Rol(es)', border: 'T', align: 'L', ln: 0);
+        $this->MultiCell(w: 40, h: 0, border: 'T', align: 'L', ln: 0, txt: 'Rol(es)');
         $this->setFont(family: 'iosevkafixedss12', size: 10);
-        $this->MultiCell(w: 0, h: 0, txt: $filters['roles'] ?: 'Todos', border: 'T', ln: 1);
+        $this->MultiCell(w: 0, h: 0, align: 'L', border: 'T', ln: 1, txt: $filters['roles'] ?: 'Todos');
         $this->setFont(family: 'helvetica', style: 'B', size: 10);
-        $this->MultiCell(w: 40, h: 0, txt: 'Operación', border: 'T', align: 'L', ln: 0);
+        $this->MultiCell(w: 40, h: 0, border: 'T', align: 'L', ln: 0, txt: 'Operación');
         $this->setFont(family: 'iosevkafixedss12', size: 10);
-        $this->MultiCell(w: 0, h: 0, txt: $filters['operations'] ?: 'Todas', border: 'T', ln: 1);
+        $this->MultiCell(w: 0, h: 0, align: 'L', border: 'T', ln: 1, txt: $filters['operations'] ?: 'Todas');
 
+        $this->setLineStyle([
+            'width' => 0.75 / $this->k,
+            'cap' => 'butt',
+            'join' => 'mitter',
+            'dash' => 0,
+            'color' => [0, 0, 0],
+        ]);
         $this->setFont(family: 'helvetica', style: 'B', size: 10);
         $this->setFillColor(0, 53, 41);
         $this->setTextColor(255, 255, 255);
         $this->Cell(w: 0, txt: '2. DETALLE DE LAS TRAZAS DE ACTIVIDADES', border: 0, ln: 1, fill: true);
         $this->setTextColor(0, 0, 0);
-        $this->MultiCell(w: 30, h: 5, align: 'L', ln: 0, txt: 'Fecha Creado',);
-        $this->MultiCell(w: 10, h: 5, align: 'L', ln: 0, txt: 'ID',);
-        $this->MultiCell(w: 64, h: 5, align: 'L', ln: 0, txt: 'Permiso',);
-        $this->MultiCell(w: 40, h: 5, align: 'L', ln: 0, txt: 'Rol/es',);
-        $this->MultiCell(w: 40, h: 5, align: 'L', ln: 0, txt: 'Usuario/s',);
-        $this->MultiCell(w: 65, h: 5, align: 'L', ln: 1, txt: 'Operación',);
+        $this->MultiCell(w: 30, h: 5, align: 'L', ln: 0, border: 'B', txt: 'Fecha Creado', );
+        $this->MultiCell(w: 10, h: 5, align: 'L', ln: 0, border: 'B', txt: 'ID', );
+        $this->MultiCell(w: 64, h: 5, align: 'L', ln: 0, border: 'B', txt: 'Permiso', );
+        $this->MultiCell(w: 40, h: 5, align: 'L', ln: 0, border: 'B', txt: 'Rol/es', );
+        $this->MultiCell(w: 40, h: 5, align: 'L', ln: 0, border: 'B', txt: 'Usuario/s', );
+        $this->MultiCell(w: 65, h: 5, align: 'L', ln: 1, border: 'B', txt: 'Operación', );
 
         // establece el margen superior a la altura ocupada por el header
         $this->tMargin = $this->GetY();
@@ -97,7 +103,7 @@ class ExportIndexToPdf extends BasePdf
         $this->setHeaderMargin();
         $this->setFooterMargin();
 
-        $this->setAutoPageBreak(TRUE);
+        $this->setAutoPageBreak(true, 10);
 
         $this->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
@@ -107,28 +113,47 @@ class ExportIndexToPdf extends BasePdf
 
         $this->AddPage();
 
-        $permissions = Permission::filter($this->filters)
-            ->with(['users', 'roles'])
-            ->get();
+        $permissions = Permission::filter($this->filters)->get();
 
-        foreach ($permissions->chunk(15) as $index => $chunk)
+        foreach ($permissions as $permission)
         {
-            if ($index > 0)
+            $ts = $permission->created_at->isoFormat('L LTS');
+            $heightTs = ceil($this->getStringHeight(30, $ts, border: 'B'));
+            $heightId = ceil($this->getStringHeight(10, $permission->id ?? '', border: 'B'));
+            $heightPermission = ceil($this->getStringHeight(64, "[{$permission->name}] {$permission->description}", border: 'B'));
+            $heightRoles = ceil($this->getStringHeight(40, $permission->roles()->pluck('name')->implode(', '), border: 'B'));
+            $heightUsers = ceil($this->getStringHeight(40, $permission->users()->pluck('email')->implode(', '), border: 'B'));
+            $heightOperations = ceil($this->getStringHeight(65, $this->getOperation($permission), border: 'B'));
+
+            $maxHeight = max($heightId, $heightOperations, $heightPermission, $heightRoles, $heightTs, $heightUsers, 0);
+
+            $txtRoles = $permission->roles()->pluck('name')->implode(', ');
+            $txtUsers = $this->getUsers($permission);
+
+            $this->startTransaction();
+            $startPage = $this->getPage();
+            $this->MultiCell(w: 30, h: $maxHeight, maxh: $maxHeight, border: 'B', align: 'L', valign: 'T', ln: 0, txt: $ts);
+            $this->MultiCell(w: 10, h: $maxHeight, maxh: $maxHeight, border: 'B', align: 'L', valign: 'T', ln: 0, txt: $permission->id ?? '');
+            $this->MultiCell(w: 64, h: $maxHeight, maxh: $maxHeight, border: 'B', align: 'L', valign: 'T', ln: 0, txt: "[{$permission->name}] {$permission->description}");
+            $this->MultiCell(w: 40, h: $maxHeight, maxh: $maxHeight, border: 'B', align: 'L', valign: 'T', ln: 0, txt: $txtRoles);
+            $this->MultiCell(w: 40, h: $maxHeight, maxh: $maxHeight, border: 'B', align: 'L', valign: 'T', ln: 0, txt: $txtUsers);
+            $this->MultiCell(w: 65, h: $maxHeight, maxh: $maxHeight, border: 'B', align: 'L', valign: 'T', ln: 1, txt: $this->getOperation($permission));
+
+            if ($this->getPage() > $startPage)
             {
+                $this->rollbackTransaction(true);
                 $this->AddPage();
+                $this->MultiCell(w: 30, h: $maxHeight, maxh: $maxHeight, border: 'B', align: 'L', valign: 'T', ln: 0, txt: $ts);
+                $this->MultiCell(w: 10, h: $maxHeight, maxh: $maxHeight, border: 'B', align: 'L', valign: 'T', ln: 0, txt: $permission->id ?? '');
+                $this->MultiCell(w: 64, h: $maxHeight, maxh: $maxHeight, border: 'B', align: 'L', valign: 'T', ln: 0, txt: "[{$permission->name}] {$permission->description}");
+                $this->MultiCell(w: 40, h: $maxHeight, maxh: $maxHeight, border: 'B', align: 'L', valign: 'T', ln: 0, txt: $txtRoles);
+                $this->MultiCell(w: 40, h: $maxHeight, maxh: $maxHeight, border: 'B', align: 'L', valign: 'T', ln: 0, txt: $txtUsers);
+                $this->MultiCell(w: 65, h: $maxHeight, maxh: $maxHeight, border: 'B', align: 'L', valign: 'T', ln: 1, txt: $this->getOperation($permission));
             }
-
-            $chunk->each(function (Permission $permission)
+            else
             {
-                $ts = $permission->created_at->isoFormat('L LTS');
-
-                $this->MultiCell(w: 30, h: 10, maxh: 10, fitcell: true, border: 'B', align: 'L', valign: 'M', ln: 0, txt: $ts,);
-                $this->MultiCell(w: 10, h: 10, maxh: 10, fitcell: true, border: 'B', align: 'L', valign: 'M', ln: 0, txt: $permission->id ?? '',);
-                $this->MultiCell(w: 64, h: 10, maxh: 10, fitcell: true, border: 'B', align: 'L', valign: 'M', ln: 0, txt: "[{$permission->name}] {$permission->description}",);
-                $this->MultiCell(w: 40, h: 10, maxh: 10, fitcell: true, border: 'B', align: 'L', valign: 'M', ln: 0, txt: $permission->roles()->pluck('name')->implode(', '),);
-                $this->MultiCell(w: 40, h: 10, maxh: 10, fitcell: true, border: 'B', align: 'L', valign: 'M', ln: 0, txt: $permission->users()->pluck('email')->implode(', '),);
-                $this->MultiCell(w: 65, h: 10, maxh: 10, fitcell: true, border: 'B', align: 'L', valign: 'M', ln: 1, txt: $this->getOperation($permission),);
-            });
+                $this->commitTransaction();
+            }
         }
 
         return $this->Output('REPORTE: PERMISOS');
@@ -172,9 +197,10 @@ class ExportIndexToPdf extends BasePdf
         return $filters;
     }
 
-    private function getOperation(Permission $permission) : string
+    private function getOperation(Permission $permission): string
     {
-        return match (true) {
+        return match (true)
+        {
             Str::contains($permission->name, 'create') => 'Creación',
             Str::contains($permission->name, 'read') => 'Lectura',
             Str::contains($permission->name, 'update') => 'Actualización',
@@ -185,5 +211,20 @@ class ExportIndexToPdf extends BasePdf
             Str::contains($permission->name, 'restore') => 'Restauración',
             default => 'Desconocida',
         };
+    }
+
+    private function getUsers(Permission $permission): string
+    {
+        $usersByDirectPermission = $permission->users()->pluck('email')->implode(', ');
+        $usersByRoles = $permission->roles->map(
+            fn(Role $role) => $role->users->pluck('email')->implode(', ')
+        )->implode(', ');
+
+        $users = trim("{$usersByDirectPermission}, {$usersByRoles}", ', ');
+        // $users = explode(', ', $users);
+        // $users = array_unique($users);
+        // $users = implode(', ', $users);
+
+        return $users;
     }
 }
